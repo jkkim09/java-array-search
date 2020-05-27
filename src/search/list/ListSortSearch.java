955e49ce5231cacfe0021dd2e8e34e8623be1d86
jkkim09 <wkrud203@gmail.com>
2020-05-27 13:34:52 +0900

    2진 트리 정렬, 검색

--- src/search/list/ListSortSearch.java
+++ src/search/list/ListSortSearch.java
@@ -1,28 +1,69 @@
 package search.list;
 
 public class ListSortSearch implements SortSearch{
-	public int[] list;
-
+	private Node root = null;
+	
 	public ListSortSearch(int number) {
-		list = new int[number];
-		
 		for (int a=0; a<number; a++) {
 			int negativeCheck = (int)(Math.random() * 2);
 			int randomInt = (int)(Math.random() * number);
 			int n = negativeCheck == 0 ? randomInt * -1 : randomInt;
-			list[a] = n;
-		}
-		
-		for (int c : list) {
-			System.out.println(c);
+			insertValue(n);
 		}
 	}
 	
-
+    public Node insertNode(Node root, int value) {
+        Node parentNode = root;
+        Node newNode = new Node(value);
+        
+        if(parentNode==null){
+            return newNode;
+        }else if(parentNode.data>newNode.data){
+            parentNode.left = insertNode(parentNode.left, value);
+            return parentNode;
+        }else if(parentNode.data<newNode.data){
+            parentNode.right = insertNode(parentNode.right, value);
+            return parentNode;
+        }else{ 
+        	parentNode.plusCount();
+            return parentNode;
+        }
+    }
+	
+    public void insertValue(int value){
+        root = insertNode(root, value);
+    }
+    
+    public void inorder(Node root){
+        if(root!=null){
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+    
+    public void printBST(){
+        inorder(root);
+        System.out.println();
+    }   
+    
+    public Node searchNodeData(int value){
+    	Node parentNode = root;
+        while(parentNode != null){
+            if(value < parentNode.data) {
+            	parentNode = parentNode.left;
+            } else if(value > parentNode.data) {
+            	parentNode = parentNode.right;
+            } else {
+            	return parentNode;
+            }
+        }
+        return parentNode;
+    }
+    
 	@Override
 	public boolean exists(int n) {
-		// TODO Auto-generated method stub
-		return false;
+		return searchNodeData(n) != null ? true : false ;
 	}
 
 	@Override
@@ -52,5 +93,7 @@ public class ListSortSearch implements SortSearch{
 	
 	public static void main(String[] args) {
 		ListSortSearch lss = new ListSortSearch(10);
+		lss.printBST();
+		System.out.println(lss.exists(5));
 	}
 }
--- src/search/list/Node.java
+++ src/search/list/Node.java
@@ -1,5 +1,38 @@
 package search.list;
 
 public class Node {
-
+    int data;
+    int count = 0;
+    String nodeName;
+    Node left;
+    Node right;
+    
+    public Node(){
+        this.left = null;
+        this.right = null;
+    }
+    
+    public Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.count++;
+        setNodeName(data);
+    }
+    
+    public void setNodeName (int data) {
+    	this.nodeName = data + " Node";
+    }
+    
+    public String getNodeName () {
+    	return this.nodeName;
+    }
+    
+    public void plusCount() {
+    	this.count++;
+    }
+    
+    public int getCount() {
+    	return this.count;
+    }
 }
