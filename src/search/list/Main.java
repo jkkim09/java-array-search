99d7990349fd7b8c9787b6b59f0f360d1a64d64d
jakyoung <wkrud203@gmail.com>
2020-05-28 21:23:01 +0900

    최종 정리

--- src/search/list/Main.java
+++ src/search/list/Main.java
@@ -1,12 +1,67 @@
 package search.list;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
-		ListSortSearch lss = new ListSortSearch(1000000);
+		/**
+		 * 정수형 난수가 -2147483648 ~ 2147483647 (21억) 이여서 개발 시 확인 하기 힘들어 설정을
+		 * 추가하였습니다.
+		 * true : 개발 모드
+		 */
+		ListSortSearch lss = new ListSortSearch(1000000, true);
 		
-		System.out.println("start");
-//		lss.filter("3*");
-//		lss.printBST();
-		lss.range(-12, 15);
+		/**
+		 * 주석을 풀면 정렬된 값을 볼 수 있습니다 
+		 * 다만 정수 개수가 너무 크면 System.out.println가 동기 방식이기 때문에 시간이 오래 걸립니다.
+		 */
+//		lss.nodePringOut();
+		
+		/**
+		 * 1. exists (n: Int) => Boolean : 해당 값이  존재 유무
+		 */
+		
+		System.out.println(lss.exists(12));
+		
+		/**
+		 * 2. range(-10, 100) : -10 ~ 100사이값 수집 (중복된 값도 모두 수집)
+		 */
+		
+//		ArrayList<Integer> al = lss.range(-10, 10);
+//		for (int a : al) {
+//			System.out.println(a);
+//		}
+		
+		/**
+		 * 3. count([3, 90, -10, 20]) : [0, 10, 20, 0] 각 수어진 수의 갯수 수집
+		 */
+		
+//		int[] list = {3, 90, -10, 20};
+//		int[] al = lss.count(list);
+//		for(int a : al) {
+//			System.out.println(a);
+//		}
+		
+		/**
+		 * 4. take(10) : 최대 값 10개 수집 (중복시 다음값 수집해서 총 10개)
+		 */
+		
+//		int[] al = lss.take(3);
+//		for (int a : al) {
+//			System.out.println(a);
+//		}
+		
+		/**
+		 * 5. filter("1*") : 10진수 n로 시작하는 수 수집 (1, 100, 1000, -1, -1000 등등)
+		 * 6. filter("*3") : 10진수 n로 끝난는 수 수집 (3, 203, 33, -3, -4332320003 등등)
+		 * 7. filter("5")  : 10진수 자릿수 수 수집 (10000, -20032, 98033, -43011, ...)
+		 */
+		
+//		ArrayList<Integer> al = lss.filter("1*");
+//		ArrayList<Integer> al = lss.filter("*3");
+//		ArrayList<Integer> al = lss.filter("2");
+//		for (int a : al) {
+//			System.out.println(a);
+//		}
 	}
 }
--- src/search/list/Node.java
+++ src/search/list/Node.java
@@ -1,31 +1,59 @@
 package search.list;
 
 public class Node {
-    int data;
-    int count = 1;
-    Node left;
-    Node right;
+    private int data;
+    private int count = 1;
+    private Node left;
+    private Node right;
     
-    public Node(){
+	public Node(){
         this.left = null;
         this.right = null;
     }
     
-    public Node(int data){
+	public Node(int data){
         this.data = data;
         this.left = null;
         this.right = null;
     }
     
+    public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public int getCount() {
+		return this.count;
+	}
+	
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
     public void plusCount() {
     	this.count++;
     }
     
-    public int getCount() {
-    	return this.count;
-    }
-    
-    @Override
+    public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+	
+    public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	@Override
     public String toString() {
     	return Integer.toString(this.data);
     }
